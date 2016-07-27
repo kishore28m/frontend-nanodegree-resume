b1c0ea61215641034cc2a71a7065d19c0c3b9e1c
kishore <kishorechandrasekharan@kishores-Air.fios-router.home>
2016-07-26 23:17:47 -0400

    first Commit

--- js/helper.js
+++ js/helper.js
@@ -1,27 +1,107 @@
 /*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
+This file contains all of the code running in the background that makes resumeBuilder.js possible.
 These are HTML strings. As part of the course, you'll be using JavaScript functions
 replace the %data% placeholder text you see in them.
 */
+
+var name = "Kishore Chandrasekharan";
+var role ="Front-End Developer";
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
+var formattedName = HTMLheaderName.replace("%data%",name);
+var Role = HTMLheaderRole.replace("%data%",role);
+
+var bio = {
+  "Name" : name,
+  "Role" : role,
+  "ContactInfo" : {
+    "email" : "xyz@gmail.com",
+    "mobile" : "123-456-7890"
+  },
+  "location" : "New York, NY",
+  "welcomemessage" : "Welcome to my Interative resume",
+  "skills" : [
+    "HTML,", "CSS,", "JavaScript,", "SQL,", "SAS,", "Business Analysis,",
+    "Project Management."
+  ],
+  "pic" : ("images/fry.jpg")
+};
+
+var education = {
+  "schools" : [
+    {
+      "name" : "Fairleigh Dickinson University",
+      "location" : "Teaneck, New Jersey",
+      "degree" : "Masters",
+      "major" : "Electrical Engineering",
+      "dates" : "August 2001 - May 2003",
+      "url" : "http://www.fdu.edu"
+    },
+    {
+      "name" : "University of Madras",
+      "location" : "Chennai, India",
+      "degree" : "Bachelors",
+      "major" : "Electrical Engineering",
+      "dates" : "August 1997 - May 2001",
+      "url" : "http://www.unom.ac.in/"
+    }
+ ]
+};
+
+var work = {
+  "jobs" : [
+  {
+    "companyname" : "ABC industries",
+    "location" : "New York, NY",
+    "position" : "Front-end Developer",
+    "dates": "2016 - Current",
+    "url": "https://github.com/kishore28m",
+    "description": "Create stunning User Experiences, using the skills learnt @ Udacity."
+  },
+  {
+    "companyname" : "XYZ Corp",
+    "location" : "New York, NY",
+    "position" : "Front-end Developer",
+    "dates": "2004 - 2016",
+    "url": "https://github.com/kishore28m",
+    "description": "Create stunning User Experiences, using the skills learnt @ Udacity."
+  }
+]
+};
+
+var projects = {
+  "fend" : [ {
+    "name" : "My Portfolio",
+    "dates" : "June 2016",
+    "description": "This was my first project in the Front-end Nanodegree program. Bootstrap was utilized for the layout and Modals was used to describe the featured work section on the website.",
+    "url" : "http://kishore28m.github.io/MyPortfolio/",
+    "pic" : ("images/Portfolio.jpg")
+  },
+  {
+    "name" : "Interactive Resume",
+    "dates" : "July 2016",
+    "description": "This was my second project in the Front-end Nanodegree program. JavaScript was used to build this Interactive resume.",
+    "url" : "#",
+    "pic" : ("images/resume.jpg")
+  }
+  ]
+};
+
+var onlineclasses = {
+    "title" : "Front-End Web Developer Nanodegree",
+    "titleUrl" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001",
+    "dates" : "2016",
+    "school" : "Udacity",
+    "url": "http://www.udacity.com"
+}
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile:</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email:</span><span class="white-text">%data%</span></li>';
+var HTMLlinkedin = '<li class="flex-item"><span class="orange-text">LinkedIn:</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github:</span><span class="white-text">%data%</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location:</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
@@ -34,13 +114,13 @@ var HTMLworkEmployer = '<a href="#">%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
+var HTMLworkDescription = '<ul><br>%data%</ul>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<img src="%data%" class="projectpic">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
@@ -49,7 +129,7 @@ var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineClasses = '<h3 class="online-entry">Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
@@ -110,9 +190,9 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
+  /*
   For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
+  appended to #mapDiv in resumeBuilder.js.
   */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
@@ -127,11 +207,12 @@ function initializeMap() {
     var locations = [];
 
     // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
+    //locations.push(bio.contacts.location);
+    locations.push(bio.location);
 
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
     education.schools.forEach(function(school){
       locations.push(school.location);
@@ -139,7 +220,7 @@ function initializeMap() {
 
     // iterates through work locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
     work.jobs.forEach(function(job){
       locations.push(job.location);
@@ -239,11 +320,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
